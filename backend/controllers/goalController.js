ee7afefda84ea374b4c8f6c2e120f75ce5dff923
Asma-Abed <abed.asma@hotmail.com>
2023-03-16 11:57:52 +0100

    adding async handler

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,10 +1,12 @@
-const getGoals = (req, res) => {
+const asyncHandler = require('express-async-handler');
+
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: 'Get goals',
   });
-};
+});
 
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error('Please add a text field!');
@@ -13,19 +15,19 @@ const setGoal = (req, res) => {
   res.status(200).json({
     message: 'Set goal',
   });
-};
+});
 
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: `Update goal ${req.params.id}`,
   });
-};
+});
 
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: `Delete goal ${req.params.id}`,
   });
-};
+});
 
 module.exports = {
   getGoals,
