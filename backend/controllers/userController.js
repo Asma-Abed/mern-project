81e5799182e82cdb39e8ecd8a9c9e601100cc7bc
Asma-Abed <abed.asma@hotmail.com>
2023-06-02 14:04:17 +0100

    jwt token generated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -33,6 +33,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -50,6 +51,7 @@ const loginUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -60,6 +62,10 @@ const loginUser = asyncHandler(async (req, res) => {
 const getMe = asyncHandler(async (req, res) => {
   res.json({ message: 'Get user data' });
 });
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+};
 
 module.exports = {
   registerUser,
