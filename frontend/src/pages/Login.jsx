299bf7aeb164f4e85a0cb17d4bcaa0b8bb2ce4fd
Asma-Abed <abed.asma@hotmail.com>
2023-06-04 15:41:48 +0100

    login is working

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,9 @@
 import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { login, reset } from '../features/auth/authSlice';
+import Spinner from '../components/Spinner';
 import { FaSignInAlt } from 'react-icons/fa';
 
 const Login = () => {
@@ -9,8 +14,30 @@ const Login = () => {
 
   const { email, password } = formData;
 
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    if (isSuccess || user) {
+      navigate('/');
+    }
+
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
   const onSubmit = (e) => {
     e.preventDefault();
+    const userData = { email, password };
+
+    dispatch(login(userData));
   };
 
   const onChange = (e) => {
@@ -19,6 +46,10 @@ const Login = () => {
       [e.target.name]: e.target.value,
     }));
   };
+
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <>
