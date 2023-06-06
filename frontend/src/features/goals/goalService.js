b16dd8e838b94244d6a782f627c5b332080ad54b
Asma-Abed <abed.asma@hotmail.com>
2023-06-06 13:24:00 +0100

    goal extra reducer added

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 const API_URL = '/api/goals/';
 
-const createGoal = (goalData, token) => {
+const createGoal = async (goalData, token) => {
   const config = {
     headers: {
       Authorization: `Bearer ${token}`,
     },
   };
 
-  const res = axios.post(API_URL, goalData, config);
+  const res = await axios.post(API_URL, goalData, config);
 
   return res.data;
 };
